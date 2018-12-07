8fea41b4997c2fa582a54ab53739fff11e91a6f8
Liam MacPherson <b5024059@my.shu.ac.uk>
2018-12-07 01:38:12 +0000

    Added the ability to sign in to the app through the GPGS
    Added the ability to sign into the application through the GPGS.
    Changed to make the code cleaner.
    Changes to the MainMenuFragment UI to ake it more appealing.

--- app/src/main/java/shu/apps/eyespy/MainActivity.java
+++ app/src/main/java/shu/apps/eyespy/MainActivity.java
@@ -1,23 +1,37 @@
 package shu.apps.eyespy;
 
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.games.Games;
+import com.google.android.gms.games.Player;
+import com.google.android.gms.games.PlayersClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import java.util.Objects;
 
 //TODO: Rearrange the drawable folder to be more organised.
 
 public class MainActivity extends FragmentActivity implements
         MainMenuFragment.Listener {
 
-    private MainMenuFragment mMainMenuFragment;
-
-    private GoogleSignInClient mGoogleSignInClient;
-
     private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int REQUEST_CODE_SIGN_IN = 9001;
+    private MainMenuFragment mMainMenuFragment;
+    private GoogleSignInClient mGoogleSignInClient;
+    private PlayersClient mPlayersClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,28 +51,13 @@ public class MainActivity extends FragmentActivity implements
     }
 
     @Override
-    public void onProfilePageRequested() {
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume()");
 
-    }
-
-    @Override
-    public void onStartGameRequested() {
-
-    }
-
-    @Override
-    public void onShowAchievementsRequested() {
-
-    }
-
-    @Override
-    public void onShowLeaderboardRequested() {
-
-    }
-
-    @Override
-    public void onShowSettingsRequested() {
-
+        // State of signed in user could change when changing from another app,
+        // try and sign in again when the app resumes.
+        signInSilently();
     }
 
     // Switch UI to the given fragment.
@@ -72,6 +71,119 @@ public class MainActivity extends FragmentActivity implements
     // Returns whether the user is currently signed into a Google account.
     private boolean isSignedIn() {
         return GoogleSignIn.getLastSignedInAccount(this) != null;
+    }
+
+    private void signInSilently() {
+        Log.d(TAG, "signInSilently()");
+
+        mGoogleSignInClient.silentSignIn().addOnCompleteListener(this,
+                new OnCompleteListener<GoogleSignInAccount>() {
+                    @Override
+                    public void onComplete(@NonNull Task<GoogleSignInAccount> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "signInSilently(): Success.");
+                            onConnected(task.getResult());
+                        } else {
+                            Log.d(TAG, "signInSilently(): Failure.", task.getException());
+                            onDisconnected();
+                        }
+                    }
+                });
+    }
+
+    private void startSignInIntent() {
+        startActivityForResult(mGoogleSignInClient.getSignInIntent(), REQUEST_CODE_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        if (requestCode == REQUEST_CODE_SIGN_IN) {
+            Task<GoogleSignInAccount> task =
+                    GoogleSignIn.getSignedInAccountFromIntent(intent);
+
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                onConnected(account);
+            } catch (ApiException e) {
+                String message = e.getMessage();
+                if (message == null || message.isEmpty()) {
+                    message = getString(R.string.sign_in_other_error);
+                }
+
+                onDisconnected();
+
+                new AlertDialog.Builder(this)
+                        .setMessage(message)
+                        .setNeutralButton(android.R.string.ok, null)
+                        .show();
+            }
+        }
+    }
+
+    private void onConnected(GoogleSignInAccount account) {
+        Log.d(TAG, "onConnected(): Connected to Google APIs.");
+
+        //TODO: Should we store the player as a class? This may allow for anonymous users?
+        mPlayersClient = Games.getPlayersClient(this, account);
+
+        mPlayersClient.getCurrentPlayer()
+                .addOnCompleteListener(new OnCompleteListener<Player>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Player> task) {
+                        if (task.isSuccessful()) {
+                            final String username = Objects.requireNonNull(task.getResult()).getDisplayName();
+                            Log.d(TAG, "onConnected(): Username - " + username);
+                            mMainMenuFragment.setUsername(username);
+                        }
+                    }
+                });
+    }
+
+    private void onDisconnected() {
+        Log.d(TAG, "onDisconnected()");
+
+        mPlayersClient = null;
+    }
+
+    @Override
+    public void onProfilePageRequested() {
+
+    }
+
+    @Override
+    public void onStartGameRequested() {
+
+    }
+
+    @Override
+    public void onShowAchievementsRequested() {
+        // If not signed in then try and sign in.
+        if (!isSignedIn()) {
+            startSignInIntent();
+        }
+
+        if (isSignedIn()) {
+            // TODO: Show Achievements Fragment
+        }
+    }
+
+    @Override
+    public void onShowLeaderboardRequested() {
+        // If not signed in then try and sign in.
+        if (!isSignedIn()) {
+            startSignInIntent();
+        }
+
+        if (isSignedIn()) {
+            // TODO: Show Leaderboard Fragment
+        }
+    }
+
+    @Override
+    public void onShowSettingsRequested() {
+
     }
 
 }
--- app/src/main/java/shu/apps/eyespy/MainMenuFragment.java
+++ app/src/main/java/shu/apps/eyespy/MainMenuFragment.java
@@ -1,18 +1,3 @@
-/* Copyright (C) 2013 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package shu.apps.eyespy;
 
 import android.os.Bundle;
@@ -23,24 +8,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class MainMenuFragment extends Fragment implements OnClickListener {
 
     private static final String TAG = MainMenuFragment.class.getSimpleName();
-
-    interface Listener {
-        void onProfilePageRequested();
-
-        void onStartGameRequested();
-
-        void onShowAchievementsRequested();
-
-        void onShowLeaderboardRequested();
-
-        void onShowSettingsRequested();
-    }
-
+    private String mUsername;
     private Listener mListener = null;
+
+    private TextView mUsernameTextView;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -61,7 +37,14 @@ public class MainMenuFragment extends Fragment implements OnClickListener {
             view.findViewById(clickableId).setOnClickListener(this);
         }
 
+        mUsernameTextView = view.findViewById(R.id.main_menu_username_text_view);
+
         return view;
+    }
+
+    public void setUsername(String username) {
+        this.mUsername = username;
+        updateUI();
     }
 
     public void setListener(Listener listener) {
@@ -87,5 +70,21 @@ public class MainMenuFragment extends Fragment implements OnClickListener {
             case R.id.main_menu_options_button:
                 mListener.onShowSettingsRequested();
         }
+    }
+
+    private void updateUI() {
+        mUsernameTextView.setText(mUsername);
+    }
+
+    interface Listener {
+        void onProfilePageRequested();
+
+        void onStartGameRequested();
+
+        void onShowAchievementsRequested();
+
+        void onShowLeaderboardRequested();
+
+        void onShowSettingsRequested();
     }
 }
