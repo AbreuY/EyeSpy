865a0d7ae254e85ba3781178855f5ad29c3a81a0
Liam MacPherson <b5024059@my.shu.ac.uk>
2018-12-20 22:10:07 +0000

    Re-merged changes from google_image_api

--- app/src/main/java/shu/apps/eyespy/fragments/CameraFragment.java
+++ app/src/main/java/shu/apps/eyespy/fragments/CameraFragment.java
@@ -96,7 +96,7 @@ public class CameraFragment extends Fragment
 
     //TODO: Change this to behave like a callback
     public interface Callback {
-        void onImageTaken(Image image);
+        void onImageTaken(File file);
     }
 
     private Item selectedItem;
@@ -850,10 +850,10 @@ public class CameraFragment extends Fragment
                 public void onCaptureCompleted(@NonNull CameraCaptureSession session,
                                                @NonNull CaptureRequest request,
                                                @NonNull TotalCaptureResult result) {
-                    showToast("Saved: " + mFile);
-
-                    Log.d(TAG, mFile.toString());
                     unlockFocus();
+                    showToast("Saved: " + mFile);
+                    mCallback.onImageTaken(mFile);
+                    Log.d(TAG, mFile.toString());
                 }
             };
 
